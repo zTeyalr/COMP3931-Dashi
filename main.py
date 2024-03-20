5e1b026c1cb3b2d821c7cfe3109ecffc8bc23a66
sc20ld <leonardodashi09@gmail.com>
2024-03-20 06:11:06 +0100

    Get information from the header file

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import os
 import data_loader
 def main():
     sample_data = 'sample_data'
-    counter = 0
+    
     for filename in os.listdir(sample_data):
         if filename.endswith('.mat'):
-            counter+=1
             
             mat_file = os.path.join(sample_data, filename)
             hea_file = mat_file.replace('.mat', '.hea')
@@ -17,14 +16,11 @@ def main():
                 ecg_dict = data_loader.load_mat(mat_file)
                 metadata = data_loader.load_header(hea_file)
 
-                print("ECG Data:", ecg_dict)
-                print("Metadata:", metadata)
-
             except FileNotFoundError:
                 print(f"Warning: Corresponding file not found for {filename}")
-    
-    print(counter)
+     
     return
+
 
 if __name__ == "__main__":
     main()
