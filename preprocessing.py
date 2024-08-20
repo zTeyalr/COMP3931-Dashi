5f8a1a44c1e28ec325b29902d3970b04c898b192
Leonardo Dashi <sc20ld@leeds.ac.uk>
2024-08-20 11:34:09 +0200

    Butterworth filters added, general implementation

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,27 @@
 """"
     Preprocessing phase consisting of filtering, resampling, normalization
 """
-from scipy.signal import butter, sosfiltfilt, sosfreqz
+from scipy.signal import butter, sosfiltfilt
 
 def butterworth_highpass(ecg_data, cutoff, ecg_frequency, order=3, filter_type='highpass'):
     """"
     Butterworth highpass filter for baseline wander removal
     """
-    nyquist_frequency = 0.5 * ecg_frequency #nyquist frequency explanation
+    nyquist_frequency = 0.5 * ecg_frequency #nyquist frequency to prevent aliasing
     norm_cutoff = cutoff / nyquist_frequency
     sos = butter(order, norm_cutoff, btype=filter_type, analog=False, output='sos')
-    filtered_sos = sosfiltfilt(sos, ecg_data, axis=0)
+    filtered_sos = sosfiltfilt(sos, ecg_data)
 
     return filtered_sos
 
 def butterworth_bandpass(ecg_data, cutoff, ecg_frequency, order=3, filter_type='bandpass'):
     """"
-    Butterworth bandpass filter
+    Butterworth bandpass filter to remove noise and artifacts
     """
-    nyquist_frequency = 0.5 * ecg_frequency #nyquist frequency explanation
+    nyquist_frequency = 0.5 * ecg_frequency #nyquist frequency to prevent aliasing
     norm_cutoff = [cutoff[0] / nyquist_frequency, cutoff[1] / nyquist_frequency]
     sos = butter(order, norm_cutoff, btype=filter_type, analog=False, output='sos')
-    filtered_sos = sosfiltfilt(sos, ecg_data, axis=0)
+    filtered_sos = sosfiltfilt(sos, ecg_data)
 
     return filtered_sos
 
